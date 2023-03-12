5c6cbaf1b1f0f3848cbd795c70efd9f8769ad7e9
Marco <mail@noetel.com>
2023-03-12 22:07:41 +0100

    asdf

--- app.ts
+++ app.ts
@@ -19,3 +19,11 @@ eventEmitter.on('scream', myEventHandler);
 
 //Fire the 'scream' event:
 eventEmitter.emit('scream');
+/*
+var http = require('http');
+http.createServer(function (req: any, res:any) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+  }).listen(8080);
+*/
